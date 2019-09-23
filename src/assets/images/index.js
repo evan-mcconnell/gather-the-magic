cfefa9b6f5b5bf1e8b79b20cf0b27305314802d2
Kimberly McConnell <kmcconnell393@gmail.com>
2019-09-22 17:28:02 -0700

    add more symbol images and buttons to form

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,7 +3,14 @@ import blueSym from './blue_symbol.svg';
 import greenSym from './green_symbol.svg';
 import whiteSym from './white_symbol.svg';
 import redSym from './red_symbol.svg';
-import land from './land.svg';
+import land from './land_symbol.svg';
+import artifact from './artifact_symbol.svg';
+import creature from './creature_symbol.svg';
+import enchantment from './enchantment_symbol.svg';
+import instant from './instant_symbol.svg';
+import planeswalker from './planeswalker_symbol.svg';
+import sorcery from './sorcery_symbol.svg';
+import searchIcon from './search_icon.svg';
 
 
 const colorSym = {
@@ -14,7 +21,18 @@ const colorSym = {
   red: redSym
 }
 
+const typeSym = {
+  artifact: artifact,
+  creature: creature,
+  enchantment: enchantment,
+  instant: instant,
+  planeswalker: planeswalker,
+  sorcery: sorcery,
+}
+
 export default {
   colorSym: colorSym,
-  land: land
+  typeSym: typeSym,
+  land: land,
+  searchIcon: searchIcon,
 }
--- src/components/screens/search/SearchForm.jsx
+++ src/components/screens/search/SearchForm.jsx
@@ -8,12 +8,21 @@ import images from './../../../assets/images/index';
 function SearchForm(props) {
 
   const {dispatch} = props
+  const [buttonWhite, toggleButtonWhite] = useState(true)
+  const [buttonBlue, toggleButtonBlue] = useState(true)
   const [buttonBlack, toggleButtonBlack] = useState(true)
+  const [buttonRed, toggleButtonRed] = useState(true)
+  const [buttonGreen, toggleButtonGreen] = useState(true)
+  const [buttonCreature, toggleButtonCreature] = useState(true)
+  const [buttonPlaneswalker, toggleButtonPlaneswalker] = useState(true)
+  const [buttonInstant, toggleButtonInstant] = useState(true)
+  const [buttonSorcery, toggleButtonSorcery] = useState(true)
+  const [buttonEnchantment, toggleButtonEnchantment] = useState(true)
+
+  const [buttonLand, toggleButtonLand] = useState(true)
 
   let _keyword = null;
 
-
-  console.log(land)
 
   function handleSearchSubmit(e) {
     e.preventDefault();
@@ -29,46 +38,95 @@ function SearchForm(props) {
           id='keyword'
           placeholder='Search'
           ref={(input) => { _keyword = input; }} /> <br />
+        <div className='colorToggles'>
+          <RoundToggle onClick={() => toggleButtonWhite(!buttonWhite)}
+            active={buttonWhite}
+            colorActive='lightgrey'
+            colorInactive='grey'>
+            <img src={images.colorSym.white}></img></RoundToggle>
 
-        <RoundToggle onClick={() => toggleButtonWhite(!buttonWhite)}
-          active={buttonWhite}
-          colorActive='lightgrey'
-          colorInactive='grey'>
-          <img src={images.colorSym.white}></img></RoundToggle>
+          <RoundToggle onClick={() => toggleButtonBlue(!buttonBlue)}
+            active={buttonBlue}
+            colorActive='lightgrey'
+            colorInactive='grey'>
+            <img src={images.colorSym.blue}></img></RoundToggle>
 
-        <RoundToggle onClick={() => toggleButtonBlue(!buttonBlue)}
-          active={buttonBlue}
-          colorActive='lightgrey'
-          colorInactive='grey'>
-          <img src={images.colorSym.blue}></img></RoundToggle>
+          <RoundToggle onClick={() => toggleButtonBlack(!buttonBlack)}
+            active={buttonBlack}
+            colorActive='lightgrey'
+            colorInactive='grey'>
+            <img src={images.colorSym.black}></img></RoundToggle>
 
-        <RoundToggle onClick={() => toggleButtonBlack(!buttonBlack)}
-          active={buttonBlack}
-          colorActive='lightgrey'
-          colorInactive='grey'>
-          <img src={images.colorSym.black}></img></RoundToggle>
+          <RoundToggle onClick={() => toggleButtonRed(!buttonRed)}
+            active={buttonRed}
+            colorActive='lightgrey'
+            colorInactive='grey'>
+            <img src={images.colorSym.red}></img></RoundToggle>
 
-        <RoundToggle onClick={() => toggleButtonRed(!buttonRed)}
-          active={buttonRed}
-          colorActive='lightgrey'
-          colorInactive='grey'>
-          <img src={images.colorSym.red}></img></RoundToggle>
+          <RoundToggle onClick={() => toggleButtonGreen(!buttonGreen)}
+            active={buttonGreen}
+            colorActive='lightgrey'
+            colorInactive='grey'>
+            <img src={images.colorSym.green}></img></RoundToggle>
 
-        <RoundToggle onClick={() => toggleButtonGreen(!buttonGreen)}
-          active={buttonGreen}
-          colorActive='lightgrey'
-          colorInactive='grey'>
-          <img src={images.colorSym.green}></img></RoundToggle>
-
-        <RoundToggle onClick={() => toggleButtonBlack(!buttonBlack)}
-          active={buttonBlack}
-          colorActive='lightgrey'
-          colorInactive='grey'>
-          <img src={images.colorSym.black}></img></RoundToggle>
+          <RoundToggle onClick={() => toggleButtonLand(!buttonLand)}
+            active={buttonLand}
+            colorActive='lightgrey'
+            colorInactive='grey'>
+            <img src={images.land}></img></RoundToggle>
+        </div>
+        <div className='typeToggles'>
+          <RoundToggle onClick={() => toggleButtonCreature(!buttonCreature)}
+            active={buttonCreature}
+            colorActive='lightgrey'
+            colorInactive='grey'>
+            <img src={images.typeSym.creature}></img></RoundToggle>
+          <RoundToggle onClick={() => toggleButtonPlaneswalker(!buttonPlaneswalker)}
+            active={buttonPlaneswalker}
+            colorActive='lightgrey'
+            colorInactive='grey'>
+            <img src={images.typeSym.planeswalker}></img></RoundToggle>
+          <RoundToggle onClick={() => toggleButtonInstant(!buttonInstant)}
+            active={buttonInstant}
+            colorActive='lightgrey'
+            colorInactive='grey'>
+            <img src={images.typeSym.instant}></img></RoundToggle>
+          <RoundToggle onClick={() => toggleButtonSorcery(!buttonSorcery)}
+            active={buttonSorcery}
+            colorActive='lightgrey'
+            colorInactive='grey'>
+            <img src={images.typeSym.sorcery}></img></RoundToggle>
+          <RoundToggle onClick={() => toggleButtonEnchantment(!buttonEnchantment)}
+            active={buttonEnchantment}
+            colorActive='lightgrey'
+            colorInactive='grey'>
+            <img src={images.typeSym.enchantment}></img></RoundToggle>
+          <RoundToggle onClick={() => toggleButtonCreature(!buttonCreature)}
+            active={buttonCreature}
+            colorActive='lightgrey'
+            colorInactive='grey'>
+            <img src={images.typeSym.creature}></img></RoundToggle>
+          <RoundToggle onClick={() => toggleButtonLand(!buttonLand)}
+            active={buttonLand}
+            colorActive='lightgrey'
+            colorInactive='grey'>
+            <img src={images.land}></img></RoundToggle>
+          
+        </div>
 
       </form>
 
-
+    <style jsx>{`
+      form {
+        width: 500px;
+        margin: auto;
+      }
+      .colorToggles, .typeToggles {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+      }
+    `}</style>
     </div>
   )
 }
